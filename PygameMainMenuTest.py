d0b750889d4fbaef26ca3ab98db837e29cf5bd71
kuman514 <7iggif@naver.com>
2016-02-03 16:16:42 +0900

    Pygame Project main menu screen and cursor input test, not done

--- PygameMainMenuTest.py
+++ PygameMainMenuTest.py
@@ -8,6 +8,7 @@ menuinput = 0
 
 pygame.init()
 Display = pygame.display.set_mode((width,height))
+TestTitle = pygame.image.load('TestTitle.png')
 pygame.display.set_caption('Pygame Main Menu Test')
 
 while True:
@@ -26,6 +27,7 @@ while True:
 		menuinput = menuinput % 3
 
 	Display.fill(0x000000)
+	Display.blit(TestTitle, (0,0))
 	pygame.draw.rect(Display, 0xff0000, (480,640+(80*menuinput),320,80), 4)
 	## Rect Tuple -> (start x, start y, width, height)
 	pygame.display.update()
